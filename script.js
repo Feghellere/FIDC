a29cc2dd9235cd67d9f9d128c03d213ebace4f9d
Feghellere <152501844+Feghellere@users.noreply.github.com>
2024-03-18 15:03:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-function verificarSenha() {
-    var senhaInserida = document.getElementById("senha").value;
-
-    // Simula a autenticação enviando a senha para um servidor
-    // Substitua isso pelo seu lógica de autenticação real
-    // Para fins de demonstração, estou usando uma senha codificada para comparação
-    var senhaCorreta = "123_mad_123";
-
-    if (senhaInserida === senhaCorreta) {
-        document.getElementById("senhaForm").style.display = "none";
-        document.getElementById("botaoSecreto").style.display = "block";
-    } else {
-        alert("Senha incorreta!");
+function calcularDigitoControle(codigo) {
+    const pesos = [2, 3, 4, 5, 6];
+    let soma = 0;
+    codigo.split('').reverse().forEach((digito, index) => {
+        soma += parseInt(digito) * pesos[index % pesos.length];
+    });
+    const resto = soma % 11;
+    let digitoControle = 11 - resto;
+    if (digitoControle >= 10) {
+        digitoControle = 0;
     }
+    return digitoControle;
 }
 
-function acaoBotao() {
-    alert("Você descobriu o botão secreto!");
-    // Adicione aqui a ação que deseja executar ao clicar no botão secreto
-}
+document.getElementById('botaoCalcular').addEventListener('click', function() {
+    const codigo = document.getElementById('campoCodigo').value;
+    if (!isNaN(codigo) && codigo !== '') {
+        const digitoControle = calcularDigitoControle(codigo);
+        document.getElementById('labelResultado').textContent = `CÓDIGO COMPLETO: ${codigo}-${digitoControle}`;
+    } else {
+        document.getElementById('labelResultado').textContent = 'INVÁLIDO';
+    }
+});
+
+document.getElementById('botaoLogica').addEventListener('click', function() {
+    alert("A lógica para calcular o dígito de controle é baseada no método 'Módulo 11'.\n"
+        + "1. Multiplica-se cada dígito do número por pesos crescentes de 2 a 6, da direita para a esquerda.\n"
+        + "2. Somam-se os resultados dessas multiplicações.\n"
+        + "3. O total é então dividido por 11 e o resto da divisão é calculado.\n"
+        + "4. O dígito de controle é determinado por 11 menos o resto da divisão.\n"
+        + "5. Se o resultado for 10 ou 11, o dígito de controle se torna 0.");
+});
